e7934548b9027d54281c614e9f83c52cc1f9a8d2
jomrembau <jomir.bautista@gmail.com>
2026-05-21 09:43:30 +0200

    Database reset

--- Day 2/Challenges/main.py
+++ Day 2/Challenges/main.py
@@ -1,12 +1,28 @@
 import typer
 from rich.console import Console
+from database import reset
 
-app = typer = typer.Typer()
+app = typer.Typer()
 console = Console()
 
 @app.command()
 def add_student():
     console.print("Adding a new student..")
+
+@app.command()
+def add_course():
+    console.print("Adding a new course..")
+
+@app.command()
+def reset_database():
+    answer = input("This will delete all the data. Are you sure? y/n: ")
+
+    if answer.strip().lower() == "y":
+        reset()
+        typer.echo("Database reset successful")
+    else:
+        typer.echo("Database reset aborted")
+
 
 if __name__ == "__main__":
     app()
