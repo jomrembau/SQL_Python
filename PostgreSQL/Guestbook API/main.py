67c6544de4022f7f9794892e2942cd96bb952666
jomrembau <jomir.bautista@gmail.com>
2026-05-30 10:11:16 +0200

    UUID - Generating tokens

--- PostgreSQL/Guestbook API/main.py
+++ PostgreSQL/Guestbook API/main.py
@@ -3,6 +3,7 @@ from database import Database
 from pydantic import BaseModel, EmailStr, ValidationError, SecretStr
 from utils import get_password_hash, verify_password
 from psycopg2.errors import UniqueViolation
+from uuid import uuid4
 
 app = FastAPI()
 
@@ -38,11 +39,11 @@ def register(email: str, password: SecretStr = Query(default=None, min_length=8)
 
         user = User(email=email, password=password)
         hashed_password = get_password_hash(password.get_secret_value())
-
-        # with db.conn:
-        #     db.cursor.execute("INSERT INTO users (email,password) VALUES (%s,%s)",(user.email, hashed_password))
+        token = str(uuid4())
 
         user_id = db.write('users', ['email', 'password'], [email,hashed_password])
+
+        db.write('tokens', ['token', 'user_id'], [token, user_id])
 
         return {
             "message": "User successfully created.","user_id": user_id
